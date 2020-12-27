d27e38a743744c763086c12ef17e99508ed02cf2
Daniel Niemann Hjermitslev <danielhjermitslev@protonmail.com>
2020-12-27 15:56:01 +0100

    script upload

---
+++ tempmon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+printf "%-15s%5s\n" "TIMESTAMP" "TEMP(degC)"
+printf "%20s\n" "--------------------"
+
+while true
+do
+
+	temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+	timestamp=$(date +'%s')
+	printf "%-15s%5s\n" "$timestamp" "$temp"
+	sleep 30
+done
+
